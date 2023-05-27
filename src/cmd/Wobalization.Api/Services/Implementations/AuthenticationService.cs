2ea09443083bf70a2d01c881f033d6949dbb923b
Edwin Sulaiman <edwinsulaiman20@gmail.com>
2023-05-28 01:28:04 +0700

    feat: add register and get status to authentication service

--- src/cmd/Wobalization.Api/Services/Implementations/AuthenticationService.cs
+++ src/cmd/Wobalization.Api/Services/Implementations/AuthenticationService.cs
@@ -1,8 +1,10 @@
 using FluentValidation;
 using FluentValidation.Results;
+using IdGen;
 using Kern.Error;
 using Microsoft.EntityFrameworkCore;
 using Shared.Dtos.Authentication;
+using Shared.Dtos.User;
 using Shared.Entities;
 using Shared.Entities.DatabaseContexts;
 using Wobalization.Api.Models;
@@ -14,21 +16,39 @@ public class AuthenticationService : IAuthenticationService
 {
     private readonly DatabaseContext _dbContext;
     private readonly IdentityService _identityService;
-    private readonly IValidator<InLoginDto> _validator;
+    private readonly IValidator<InLoginDto> _loginValidator;
+    private readonly IValidator<InUserAddDto> _registerValidator;
+    private readonly IdGenerator _idGenerator;
+
 
     public AuthenticationService(
         DatabaseContext dbContext,
-        IValidator<InLoginDto> validator,
-        IdentityService identityService)
+        IValidator<InLoginDto> loginValidator,
+        IdentityService identityService,
+        IValidator<InUserAddDto> registerValidator,
+        IdGenerator idGenerator)
     {
         _dbContext = dbContext;
-        _validator = validator;
+        _loginValidator = loginValidator;
         _identityService = identityService;
+        _registerValidator = registerValidator;
+        _idGenerator = idGenerator;
+    }
+
+    public async Task<ErrorBase?> GetStatusAsync()
+    {
+        var isAnyUserExist = await _dbContext.User!.AnyAsync();
+        if (isAnyUserExist)
+        {
+            return null;
+        }
+
+        return new NotFoundError("No user found, app has not been initialized");
     }
 
     public async Task<(OutTokenDto?, ValidationResult?, ErrorBase?)> SignInAsync(InLoginDto dto)
     {
-        var validationResult = _validator.Validate(dto);
+        var validationResult = _loginValidator.Validate(dto);
         if (!validationResult.IsValid)
         {
             return (null, validationResult, null);
@@ -47,6 +67,41 @@ public class AuthenticationService : IAuthenticationService
         {
             return (null, null, new BadRequestError("Username or password is wrong"));
         }
+
+        var identity = new IdentityModel
+        {
+            Id = user.Id
+        };
+
+        var token = await _identityService.SignInAsync(identity);
+        return (new OutTokenDto { Token = token }, null, null);
+    }
+
+    public async Task<(OutTokenDto?, ValidationResult?, ErrorBase?)> SignUpAsync(InUserAddDto dto)
+    {
+        var validationResult = _registerValidator.Validate(dto);
+        if (!validationResult.IsValid)
+        {
+            return (null, validationResult, null);
+        }
+
+        var isAnyUserExist = await _dbContext.User!.AnyAsync();
+        if (isAnyUserExist)
+        {
+            return (null, null, new ConflictError("Can't create an account, app has been initialized"));
+        }
+
+        var user = new User
+        {
+            Id = _idGenerator.CreateId(),
+            FullName = dto.FullName,
+            Username = dto.Username,
+            Password = BC.HashPassword(dto.Password),
+            CreatedAt = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
+        };
+
+        await _dbContext.User!.AddAsync(user);
+        await _dbContext.SaveChangesAsync();
 
         var identity = new IdentityModel
         {
--- src/cmd/Wobalization.Api/Services/Interfaces/IAuthenticationService.cs
+++ src/cmd/Wobalization.Api/Services/Interfaces/IAuthenticationService.cs
@@ -1,11 +1,14 @@
 using FluentValidation.Results;
 using Kern.Error;
 using Shared.Dtos.Authentication;
+using Shared.Dtos.User;
 
 namespace Wobalization.Api.Services.Interfaces;
 
 public interface IAuthenticationService
 {
+    public Task<ErrorBase?> GetStatusAsync();
     public Task<(OutTokenDto?, ValidationResult?, ErrorBase?)> SignInAsync(InLoginDto dto);
+    public Task<(OutTokenDto?, ValidationResult?, ErrorBase?)> SignUpAsync(InUserAddDto dto);
     public Task<ErrorBase?> SignOutAsync();
 }
